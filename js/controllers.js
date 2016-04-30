e46c0e1b9b690b136d3c11a5d752916cff254d59
J Gregory <john@johngregory.me.uk>
2016-04-30 15:35:47 +0100

    Reworks the site a bit and fills in some blanks in spec

--- js/controllers.js
+++ js/controllers.js
@@ -1,48 +1,42 @@
-var stateMachine = angular.module('stateMachine',[]);
-
-stateMachine.controller('StatesCtrl', ['$scope', '$routeParams', '$http',
-	function($scope,$routeParams,$http) {
-		$http.get('data/states.json').success(function(data) {
-			$scope.states = data;
-
-
-
-			/* Post process data to add hyperlinks */
-			
-			var rules;
-			var state_name;
-			for (var i = 0; i < $scope.states.length; i++) {
-
-				rules = $scope.states[i].rules;
-				
-				for (var j = 0; j < rules.length; j++) {
-					for (var k = 0; k < rules[j].actions.length; k++) {
-
-						/* Loop through all states - TODO this isn't efficient */
-						for (var m = 0; m < $scope.states.length; m++) {
-							state_name = $scope.states[m].state_name;
-							rules[j].actions[k]
-								= rules[j].actions[k].replace(
-										' ' + state_name  + ' state',
-										' <a href="#'+state_name+'-state">'+state_name+'</a> state'
-									);
-						}
-					}
-				}
-			}
-			
-
-			
-		});
-	}]);
+// var stateMachine = angular.module('stateMachine',[]);
+//
+// stateMachine.controller('StatesCtrl', ['$scope', '$routeParams', '$http',
+// 	function($scope,$routeParams,$http) {
+// 		$http.get('data/states.json').success(function(data) {
+// 			$scope.states = data;
+//
+// 			/* Post process data to add hyperlinks */
+// 			var rules;
+// 			var state_name;
+// 			for (var i = 0; i < $scope.states.length; i++) {
+//
+// 				rules = $scope.states[i].rules;
+//
+// 				for (var j = 0; j < rules.length; j++) {
+// 					for (var k = 0; k < rules[j].actions.length; k++) {
+//
+// 						/* Loop through all states - TODO this isn't efficient */
+// 						for (var m = 0; m < $scope.states.length; m++) {
+// 							state_name = $scope.states[m].state_name;
+// 							rules[j].actions[k]
+// 								= rules[j].actions[k].replace(
+// 										' ' + state_name  + ' state',
+// 										' <a href="#'+state_name+'-state">'+state_name+'</a> state'
+// 									);
+// 						}
+// 					}
+// 				}
+// 			}
+// 		});
+// 	}
+// ]);
 
 var syntaxReference = angular.module('syntaxReference',[]);
 
 syntaxReference.controller('SyntaxCtrl', ['$scope','$routeParams','$http',
 	function($scope,$routeParams,$http) {
-		$http.get('data/syntax.json').success(function(data) {
+		$http.get('spec/spec.json').success(function(data) {
 			$scope.syntax	 = data;
-
 		});
 	}]);
 
@@ -51,24 +45,25 @@ syntaxReference.filter('dashIt', function() {
 		return replaceSpacesWithDashes(input);
 	}
 });
+
+// Automatically turns "can contain" notes into hyperlinks to the actual section
 syntaxReference.filter('hyperlinkSyntax', function() {
-	return function(input) {		
-
-		console.log("Running");
-
+	return function(input) {
 		if (input) {
 			var to_be_linked;
 			var output = [];
 			for (var i=0; i<input.length;i++) {
 				to_be_linked = input[i];
+				if (to_be_linked == "any") {
+					return '<a href="#syntax">any</a>';
+				}
 				output[i] = '<a href="#'+replaceSpacesWithDashes(to_be_linked)+'">'+to_be_linked+'</a>';
 			}
 			return output.join(', ');
-		}	
+		}
 		return "none";
 	}
 });
-
 
 function replaceSpacesWithDashes(input) {
 	console.log("Run replace on "+input);
